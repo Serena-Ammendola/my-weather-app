db5cfcec08ff70b66c7724478f4763a1474305e5
Serena-Ammendola <serena.ammendola95@gmail.com>
2024-03-03 22:13:59 +0000

    added more javascript weather data

--- src/index.js
+++ src/index.js
@@ -2,9 +2,42 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let weatherCityElement = document.querySelector("#weather-city");
-  weatherCityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
+  weatherCityElement.innerHTML = response.data.city;
+  timeElement.innerHTML = formatDate(date);
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function formatDate(date) {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "THursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+  if (hours < 10) {
+    hours = `0${hours}`;
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+  }
+
+  return `${day} ${hours}:${minutes}`;
 }
 
 function searchCity(city) {
