265abf5d4d693e88b988540edd020b7742c9aff8
Serena-Ammendola <serena.ammendola95@gmail.com>
2024-03-08 21:18:24 +0000

    java script date adjusted

--- src/index.js
+++ src/index.js
@@ -36,9 +36,9 @@ function formatDate(date) {
   let day = days[date.getDay()];
   if (hours < 10) {
     hours = `0${hours}`;
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
   }
 
   return `${day} ${hours}:${minutes}`;
